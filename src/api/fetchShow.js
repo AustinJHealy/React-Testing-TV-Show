ff2b4bd1483d21045b33fb23b19636f96e2f31df
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-09-04 19:38:49 -0400

    formatted and deleted comments

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -6,9 +6,8 @@ export const fetchShow = () => {
       "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
     )
     .then((res) => {
-      //   setShow(res.data);
-      //   setSeasons(formatSeasons(res.data._embedded.episodes));
-      console.log(JSON.stringify(res.data._embedded.episodes));
+    
+      //console.log(JSON.stringify(res.data._embedded.episodes));
       return res;
     })
     .catch((err) => {
